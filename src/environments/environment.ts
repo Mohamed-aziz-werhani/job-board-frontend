df4d43166e1ce941112b65b33af2df023141e946
Mohamed-aziz-werhani <142012018+Mohamed-aziz-werhani@users.noreply.github.com>
2024-09-01 15:01:30 +0200

    chore: prod api link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,5 @@
-export const environment = {
-    production: false,
-    base_api: 'http://localhost:3000',
-    api_version: 'v1',
-    currentVersion: '1.0.0',
-    auth0: {
-        domain: 'https://dev-axjamzboe5y1qgar.eu.auth0.com',
-        clientId: '7Xj5cwOzlWlvvgKcMqPKRZ3SpQzhd34P',
-        audience: 'https://dev-axjamzboe5y1qgar.eu.auth0.com/api/v2/',
-        redirectUri: 'http://localhost:4200',
-    }
-};
-
-
 // export const environment = {
-//     production: true,
+//     production: false,
 //     base_api: 'http://localhost:3000',
 //     api_version: 'v1',
 //     currentVersion: '1.0.0',
@@ -24,3 +10,17 @@ export const environment = {
 //         redirectUri: 'http://localhost:4200',
 //     }
 // };
+
+
+export const environment = {
+    production: true,
+    base_api: 'https://oyster-app-q56hr.ondigitalocean.app',
+    api_version: 'v1',
+    currentVersion: '1.0.0',
+    auth0: {
+        domain: 'https://dev-axjamzboe5y1qgar.eu.auth0.com',
+        clientId: '7Xj5cwOzlWlvvgKcMqPKRZ3SpQzhd34P',
+        audience: 'https://dev-axjamzboe5y1qgar.eu.auth0.com/api/v2/',
+        redirectUri: 'http://localhost:4200',
+    }
+};
